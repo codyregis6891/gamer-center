1b1025dbd911a4cd09f13f2b6b2055fca94a695b
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-26 12:13:56 -0700

    added api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-// const userRoutes = require('./userRoutes');
-// const projectRoutes = require('./projectRoutes');
 
-// router.use('/users', userRoutes);
-// router.use('/projects', projectRoutes);
+const userRoutes = require('./userRoutes');
+
+const projectRoutes = require('./projectRoutes');
+
+router.use('/users', userRoutes);
+
+router.use('/projects', projectRoutes);
 
 module.exports = router;
