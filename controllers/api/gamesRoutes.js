b93e41cea5c6b0fcbec2354ec3c1cce507297bc4
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-29 13:36:17 -0700

    delete the route

--- controllers/api/gamesRoutes.js
+++ controllers/api/gamesRoutes.js
@@ -59,34 +59,5 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
-
-  try {
-    const projectData = await Games.destroy({
-
-      where: {
-
-        id: req.params.id,
-
-        user_id: req.session.user_id,
-
-      },
-    });
-
-    if (!projectData) {
-
-      res.status(404).json({ message: 'No game found with this id!' });
-
-      return;
-    }
-
-    res.status(200).json(projectData);
-
-  } catch (err) {
-
-    res.status(500).json(err);
-    
-  }
-});
 
 module.exports = router;
