51464c6b43d8297f4fb61613f5b2ce96d9c4c1df
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-29 13:33:56 -0700

    having the post route work

--- controllers/api/gamesRoutes.js
+++ controllers/api/gamesRoutes.js
@@ -12,9 +12,33 @@ router.get('/', async (req, res) => {
 
  });
 
+ router.get('/:id', async (req, res) => {
+
+  try {
+
+    const gameData = await Games.findByPk(req.params.id, {
+
+    });
+
+    if (!gameData) {
+
+      res.status(404).json({ message: 'No games found with this id!' });
+
+      return;
+
+    }
+
+    res.status(200).json(gameData);
+
+  } catch (err) {
+
+    res.status(500).json(err);
+
+  }
+});
 
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
 
   try {
 
