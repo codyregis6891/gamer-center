809a8d1cbc6a9e98f2b9120219b2923186e6b1e4
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-29 13:56:20 -0700

    updatign

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { User } = require('../../models');
+const { User, Games, Favorites } = require('../../models');
 
 const withAuth = require('../../utils/auth');
 
@@ -17,21 +17,18 @@ router.get('/', async (req, res) => {
  });
 
  router.get('/:id', async (req, res) => {
-
   try {
-
     const userData = await User.findByPk(req.params.id, {
+      
+      include: [{ model: Games, through: Favorites, as: 'favorited_games' }]
 
     });
-
     if (!userData) {
 
       res.status(404).json({ message: 'No user found with this id!' });
 
       return;
-
     }
-
     res.status(200).json(userData);
 
   } catch (err) {
