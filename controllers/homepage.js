bcd844e694824fb47ee315355ad2d4689b9aadfb
Cody Regis <codyregis6891@yahoo.com>
2021-03-30 13:53:07 -0700

    added genre routes and handlbars

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -48,19 +48,7 @@ router.get('/categories', async (req, res) => {
 
   try {
 
-    const gamesData = await Games.findAll({
-
-      // include: [
-
-      //   {
-      //     model: User,
-
-      //     attributes: ['name'],
-
-      //   },
-
-      // ],
-    });
+    const gamesData = await Games.findAll({});
 
     const games = gamesData.map((game) => game.get({ plain: true }));
 
@@ -78,6 +66,427 @@ router.get('/categories', async (req, res) => {
 
   }
 });
+
+router.get('/categories/sports', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Sports',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('sports', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/action-adventure', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Action/Adventure',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('actionAdventure', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/rpg', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'RPG',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('rpg', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/fps', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'FPS',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('fps', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/racing', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Racing',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('racing', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/survival', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Survival',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('survival', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/platformers', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Platformers',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('platformers', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/survival', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Survival',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('survival', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/japaneserpg', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Japanese RPG',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('japaneseRPG', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/horror', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Horror',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('horror', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/fighting', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Fighting',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('fighting', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/retro-arcade', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Retro Arcade',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('retroArcade', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/retro-snes', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Retro SNES',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('retroSNES', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/retro-nes', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Retro NES',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('retroNES', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
+router.get('/categories/retro-n64', async (req, res) => {
+
+  try {
+
+    const gamesData = await Games.findAll({
+      where: {
+        genre: 'Retro N64',
+      },
+    });
+
+    const games = gamesData.map((game) => game.get({ plain: true }));
+
+    res.render('retroN64', {
+
+      ...games,
+
+      logged_in: req.session.logged_in
+
+    });
+  } catch (err) {
+
+    res.status(500).json(err);
+    console.log(err);
+
+  }
+});
+
+
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
