ce0f16d8e556ea8031cd36bc85ced45d17388741
Cody Regis <codyregis6891@yahoo.com>
2021-03-29 18:10:40 -0700

    create a user route updated

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -36,29 +36,36 @@ router.get('/', async (req, res) => {
     console.log(err);
     res.status(500).json(err);
 
-  }
+  };
 });
 
 
 router.post('/', async (req, res) => {
 
   try {
-    const userData = await User.create(req.body);
+
+    const userData = await User.create({
+
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+
+    });
 
     req.session.save(() => {
 
-      req.session.user_id = userData.id;
-
-      req.session.logged_in = true;
+      req.session.loggedIn = true;
 
       res.status(200).json(userData);
 
     });
+
   } catch (err) {
 
-    res.status(400).json(err);
+    console.log(err);
 
-  }
+    res.status(500).json(err);
+  };
 });
 
 
